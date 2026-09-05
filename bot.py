2c54a7a9c8552dcec1614151d4a2661e4ea3779e
ily1coo-cmd <ily1coo@gmail.com>
2026-09-05 15:21:59 +0500

    Update GEMINI_API_KEY in bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ from google import genai
 # ─────────────────────────────────────────────
 BOT_TOKEN        = os.getenv("BOT_TOKEN", "8920483390:AAECgLUKMF6KClOTccqBwM7Uuv1ArFDTdzo")
 ADMIN_BOT_TOKEN  = os.getenv("ADMIN_BOT_TOKEN", "8923834508:AAEoQMf1D65gLhhOEnqlUK1NSnWfNAH9CO0")
-GEMINI_API_KEY   = os.getenv("GEMINI_API_KEY", "AQ.Ab8RN6KbgTPvDhS5yXFfIbDgzQt0eS_O80XkiThX7JFuQ_Y7kQ")
+GEMINI_API_KEY   = os.getenv("GEMINI_API_KEY", "AQ.Ab8RN6JCu4hcxWF3SepaAaJ2AjmmChs9lwVviUsSkVlWxeZn3A")
 ADMIN_USER_ID    = int(os.getenv("ADMIN_USER_ID", "5098399620"))
 RESULT_JSON_PATH = "result.json"        # путь к экспорту Telegram
 
